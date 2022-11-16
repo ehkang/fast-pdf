ff88440169197df7c10f78e17efb45a1c128b328
ehkang <55227784+ehkang@users.noreply.github.com>
2022-11-16 16:44:57 +0800

    Update fastpdf.go

--- fastpdf.go
+++ fastpdf.go
@@ -24,105 +24,98 @@ const (
 )
 
 type FastPdf struct {
-	*gopdf.GoPdf
-	Items        []PdfItem // 子项
-	templatePath string    //模板路径
-	//SetTemplate  (string) //设置模版
-	//AddBarCode   (PdfItem) //添加条码
-	//AddQrCode    (PdfItem) //添加二维码
-	//AddTable     (PdfItem) //添加方框
-	//AddText      (PdfItem) //添加文本
+	gopdf        *gopdf.GoPdf
+	Header       []PdfItem //头部元素
+	Body         []PdfItem // 正文元素
+	Footer       []PdfItem //底部元素
+	TemplatePath string    //底板路径
 }
 
 // 新建F
-func New(fontPath, templatePath string) FastPdf {
+func New(fontPath string, fastPdf FastPdf) FastPdf {
 	gp := gopdf.GoPdf{}
 	gp.Start(gopdf.Config{PageSize: *gopdf.PageSizeA4}) //595.28, 841.89 = A4
-	gp.AddPage()
-	err := gp.AddTTFFont("font", "./ZiTiQuanXinYiJiXiangSong-2.ttf")
+	err := gp.AddTTFFont("font", fontPath)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	fastPdf.gopdf = &gp
+	fastPdf.newPage() //创建第一页
+	return fastPdf
+}
 
-	if templatePath != "" {
+// 新建一页
+func (pdf *FastPdf) newPage() {
+	pdf.gopdf.AddPage()
+	if pdf.TemplatePath != "" {
 		// Import page 1
-		tpl1 := gp.ImportPage(templatePath, 1, "/MediaBox")
+		tpl1 := pdf.gopdf.ImportPage(pdf.TemplatePath, pdf.gopdf.GetNumberOfPages(), "/MediaBox")
 		// Draw pdf onto page
-		gp.UseImportedTemplate(tpl1, 0, 0, 595.28, 841.89)
+		pdf.gopdf.UseImportedTemplate(tpl1, 0, 0, 595.28, 841.89)
 	}
-	return FastPdf{
-		&gp,
-		[]PdfItem{},
-		templatePath,
-	}
+	pdf.handleItems(pdf.Header) //页头，每页都添加
+	pdf.handleItems(pdf.Footer) //页脚，每页都添加
 }
 
 // 创建新PDF文件
-func (pdf *FastPdf) CreateNewPdf(items []PdfItem) (fileBytes []byte, err error) {
-	pdf.handleItems(items)
-	return pdf.GetBytesPdf(), nil
-}
-
-// SetTemplate 设置底版
-func (pdf *FastPdf) setTemplate(path string) {
-	pdf.templatePath = path
-	// Import page 1
-	tpl1 := pdf.ImportPage(pdf.templatePath, 1, "/MediaBox")
-	// Draw pdf onto page
-	pdf.UseImportedTemplate(tpl1, 0, 0, 595.28, 841.89)
+func (pdf *FastPdf) GetPdf() (fileBytes []byte, err error) {
+	pdf.handleItems(pdf.Header)
+	pdf.handleItems(pdf.Body)
+	pdf.handleItems(pdf.Footer)
+	return pdf.gopdf.GetBytesPdf(), nil
 }
 
 // 绘制文本
 func (pdf *FastPdf) drawText(left, top float64, size int, text string) {
-	err := pdf.SetFont("font", "", size)
+	err := pdf.gopdf.SetFont("font", "", size)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	pdf.SetX(left)
-	pdf.SetY(top)
-	err = pdf.Cell(nil, text)
+	pdf.gopdf.SetX(left)
+	pdf.gopdf.SetY(top)
+	err = pdf.gopdf.Cell(nil, text)
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
 func (pdf *FastPdf) drawBarCode(left, top float64, width, height int, text string) {
-	barCodeImage := StrToBarCode(text, width, height)
+	barCodeImage := strToBarCode(text, width, height)
 	buf := new(bytes.Buffer)
 	err := jpeg.Encode(buf, barCodeImage, &jpeg.Options{Quality: 100})
 	imgH1, err := gopdf.ImageHolderByBytes(buf.Bytes())
 	if err != nil {
 		log.Fatalln(err)
 	}
-	pdf.ImageByHolder(imgH1, left, top, nil)
+	pdf.gopdf.ImageByHolder(imgH1, left, top, nil)
 }
 
 func (pdf *FastPdf) drawLine(x1, y1, x2, y2 float64) {
-	pdf.SetLineWidth(1)
-	pdf.SetLineType("")
-	pdf.Line(x1, y1, x2, y2)
+	pdf.gopdf.SetLineWidth(1)
+	pdf.gopdf.SetLineType("")
+	pdf.gopdf.Line(x1, y1, x2, y2)
 }
 
 func (pdf *FastPdf) drawTable(left, top, width, height float64, row, column int) {
-	pdf.SetLineWidth(1)
-	pdf.SetLineType("")
+	pdf.gopdf.SetLineWidth(1)
+	pdf.gopdf.SetLineType("")
 	distanceH := width / float64(row)     //横线之间的间距
 	distanceW := height / float64(column) //竖线之间的间距
 	//Pen lineColor = new Pen(p.PrtColor, 0.2f);
 	for i := 0; i < row+1; i++ {
 		//画横线
 		y := top + distanceH*float64(i)
-		pdf.Line(left, y, left+width, y)
+		pdf.gopdf.Line(left, y, left+width, y)
 	}
 	for i := 0; i < column+1; i++ {
 		//画竖线
 		x := left + distanceW*float64(i)
-		pdf.Line(x, top, x, top+float64(height))
+		pdf.gopdf.Line(x, top, x, top+float64(height))
 	}
 }
 
 func (pdf *FastPdf) drawQrCode(left, top float64, size int, text string) {
-	qrCode := StrToQrCode(text, size)
+	qrCode := strToQrCode(text, size)
 	//保存到新文件中
 	//newfile, _ := os.Create("imgs/qr.png")
 	//err = jpeg.Encode(newfile, qrCode, &jpeg.Options{Quality: 100})
@@ -137,7 +130,7 @@ func (pdf *FastPdf) drawQrCode(left, top float64, size int, text string) {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	pdf.ImageByHolder(imgH1, left, top, nil)
+	pdf.gopdf.ImageByHolder(imgH1, left, top, nil)
 }
 
 func (pdf *FastPdf) handleItems(items []PdfItem) {
@@ -187,8 +180,7 @@ func (pdf *FastPdf) handleItems(items []PdfItem) {
 				left = item.Left //左边复位
 				//再绘制数据行
 
-				for rowIndex, row := range item.TableData {
-					fmt.Println(rowIndex)
+				for _, row := range item.TableData {
 					pdf.drawLine(left, top, left, nextTop) //先画左边框 仅一行
 					for cellIndex := 0; cellIndex < len(item.TableColumn); cellIndex++ {
 						nextLeft := left + float64(item.TableColumn[cellIndex].Width)
@@ -207,7 +199,7 @@ func (pdf *FastPdf) handleItems(items []PdfItem) {
 }
 
 // 生成二维码
-func StrToQrCode(content string, size int) image.Image {
+func strToQrCode(content string, size int) image.Image {
 	qrCode, _ := qr.Encode(content, qr.M, qr.Auto)
 	// 设置图片像素大小
 	qrCode, _ = barcode.Scale(qrCode, size, size)
@@ -215,7 +207,7 @@ func StrToQrCode(content string, size int) image.Image {
 }
 
 // 生成条码
-func StrToBarCode(content string, width int, height int) image.Image {
+func strToBarCode(content string, width int, height int) image.Image {
 	barCodeCs, err := code128.Encode(content)
 	if err != nil {
 		fmt.Println(err)
